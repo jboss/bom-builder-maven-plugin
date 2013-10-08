4520b0d92943406085b8e98f1308c51b94587f6d
Paul Gier <pgier@redhat.com>
2013-10-08 18:05:43 -0500

    Import cleanup

--- src/main/java/org/jboss/maven/plugins/bombuilder/BuildBomMojo.java
+++ src/main/java/org/jboss/maven/plugins/bombuilder/BuildBomMojo.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.model.Dependency;
